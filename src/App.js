527e7f1d09bf66a4efd71dd34bb94ff62acb065d
mpollachek <mpollachek81@gmail.com>
2022-06-18 23:51:44 -0400

    about page and partners

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
               <Route path='directory' element={<CampsitesDirectoryPage />} />
               <Route path='directory/:campsiteId' 
               element={<CampsiteDetailPage /> } />
+              <Route path='about' element={<AboutPage />} />
             </Routes>
             <Footer/>
         </div>
