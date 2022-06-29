b61e558db8cf2f3a70219cdf846b31f1574131a8
mpollachek <mpollachek81@gmail.com>
2022-06-28 23:24:59 -0400

    Campsites useSelector

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { Col, Row } from 'reactstrap';
 //import DisplayCard from './DisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
@@ -6,7 +7,17 @@ import { selectFeaturedPartners } from '../partners/partnersSlice';
 import AnimatedDisplayCard from './AnimatedDisplayCard';
 
 const DisplayList = () => {
-  const items = [selectFeaturedCampsite(), selectFeaturedPromotion(), selectFeaturedPartners()];
+  // const items = [
+  //   selectFeaturedCampsite(), 
+  //   selectFeaturedPromotion(), 
+  //   selectFeaturedPartners()
+  // ];
+  const items = useSelector((state) => [
+    selectFeaturedCampsite(state),
+    selectFeaturedPromotion(state),
+    selectFeaturedPartners(state)
+  ]);
+  console.log('display items:', items);
   return (
     <Row>
       {items.map((item, idx) => {
